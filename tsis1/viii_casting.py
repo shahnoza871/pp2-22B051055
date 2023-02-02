8ebbc0539684bc03ae3279d02a58e64ae85b6031
shahnoza871 <123158715+shahnoza871@users.noreply.github.com>
2023-02-02 17:32:48 +0600

    Add files via upload

---
+++ tsis1/viii_casting.py
@@ -0,0 +1,11 @@
+xi = int(1) #1
+yi = int(5.67) #5
+zi = int("8") #8
+xf = float(1) #1.0
+yf = float(4.21) #4.21
+zf = float("3") #3.0
+tf = float("4.5") #4.5
+xs = str("s1") #s1
+ys = str(2) #2
+zs = str(3.0) #3.0
+print(xi, yi, zi, xf, yf, zf, tf, xs, ys, zs)
