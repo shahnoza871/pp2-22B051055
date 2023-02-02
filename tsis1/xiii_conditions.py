12bb4792d0bc4096bf8c455f2ce8cca4c9738d68
shahnoza871 <123158715+shahnoza871@users.noreply.github.com>
2023-02-02 17:53:25 +0600

    Add files via upload

---
+++ tsis1/xiii_conditions.py
@@ -0,0 +1,26 @@
+#ex1
+a = 330
+b = 44
+if (a>b):
+    print('a is greater than b')
+
+#ex2
+a = 33
+b = 44
+if (a>b):
+    print('a is greater than b')
+elif b>a:
+    print('a is less than b')
+
+#ex3
+a = 33
+b = 44
+if a>b:
+    print("a is greater than b")
+else:
+    print('b is greater than a')
+
+#ex4
+a = 33
+b = 44
+print('a is greater than b') if a>b else print('b is greater than a')
