92b65ba2e7229d75c7a6eec07de9a3915ecf5697
shahnoza871 <123158715+shahnoza871@users.noreply.github.com>
2023-02-03 11:57:06 +0600

    Add files via upload

---
+++ tsis2/ii_lists.py
@@ -0,0 +1,28 @@
+"""
+#ex1
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:5])
+
+#ex2
+thislist = [list(('apple', 'banana')), 'cherry', 'watermelon']
+print(thislist)
+"""
+#ex3
+"""thislist = ["apple", "banana", "cherry", "orange"]
+thislist.append('kiwi')
+thislist.insert(0, 'fruits: ')
+print(thislist)"""
+
+#ex4
+"""thislist = ["apple", "banana", "cherry", "orange"]
+thislist.remove('banana')
+print(thislist)"""
+#ex5
+"""thislist = ["apple", "banana", "cherry", "orange"]
+print(thislist[-1])"""
+#ex6
+"""thislist = ["apple", "banana", "cherry", "orange"]
+print(len(fruits))
+"""
+
+
