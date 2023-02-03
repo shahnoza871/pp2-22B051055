9232d7d0131282ff68a92c19eefae6e5abdc9c36
shahnoza871 <123158715+shahnoza871@users.noreply.github.com>
2023-02-03 14:54:38 +0600

    Add files via upload

---
+++ tsis2/vi_sets.py
@@ -0,0 +1,26 @@
+#ex1
+"""fruits = {"apple", "banana", "cherry"}
+if "apple" in fruits:
+  print("Yes, apple is a fruit!")"""
+
+#ex2
+"""fruits = {"apple", "banana", "cherry"}
+fruits.add('orange')
+print(fruits)"""
+
+#ex3
+"""fruits = {"apple", "banana", "cherry"}
+more_fruits = ["orange", "mango", "grapes"]
+fruits.update(more_fruits)
+print(fruits)
+"""
+#ex4
+"""fruits = {"apple", "banana", "cherry"}
+fruits.remove('banana')
+print(fruits)
+"""
+#ex4
+"""fruits = {"apple", "banana", "cherry"}
+fruits.discard('banana')
+print(fruits)
+"""
