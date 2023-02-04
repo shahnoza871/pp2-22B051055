a6eab4faf6888f80bccb8b209f82f8c37179b0a0
shahnoza871 <123158715+shahnoza871@users.noreply.github.com>
2023-02-04 21:40:28 +0600

    Add files via upload

---
+++ tsis2/vii_dictionaries.py
@@ -0,0 +1,49 @@
+#ex1
+"""car =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(
+car.get("model")
+)
+"""
+#ex2
+"""car =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+car["year"] = 2020
+print(car["year"])
+"""
+#ex3
+"""car = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+car["color"] = "red"
+for x in car:
+    print(x)
+"""
+#ex4
+"""car =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+car.pop("model")
+for x in car:
+    print(x)
+"""
+#ex5
+"""car =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+car.clear()
+for x in car:
+    print(x)
+"""
