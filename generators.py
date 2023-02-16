cdb9770900426d482ff1ddb02360bf987881ccf2
shahnoza871 <s_nurubloeva@kbtu.kz>
2023-02-16 20:34:15 +0600

    Add task #1 in generators.py

---
+++ generators.py
@@ -0,0 +1,13 @@
+#1
+"""import math
+def generator(n):
+    i = 1
+    while i<=int(n):
+        yield pow(i, 2)
+        i += 1
+N = input('Enter a number: ')
+x = generator(N)
+j = 1
+while j<=int(N):
+    print(next(x))
+    j+=1"""
