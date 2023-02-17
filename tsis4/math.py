25346732172fa2eeb2af20c9c529bab450e643c9
shahnoza871 <s_nurubloeva@kbtu.kz>
2023-02-17 14:10:58 +0600

    Add math.py

---
+++ tsis4/math.py
@@ -0,0 +1,25 @@
+#1 Write a Python program to convert degree to radian.
+"""d = input('Degree: ')
+r = float(d)*0.0174602
+print('Radian: '+str(r))
+"""
+#2 Write a Python program to calculate the area of a trapezoid.
+"""a = input('Base, first value: ')
+b = input('Base, second value: ')
+h = input('Height: ')
+A = (float(a) + float(b))*float(h)/2
+print(A)
+"""
+#3 Write a Python program to calculate the area of regular polygon.
+"""import math
+n = input('Input number of sides: ')
+l = input('Input the length of a side: ')
+A = pow(float(l), 2)
+print(int(A))
+"""
+#4 Write a Python program to calculate the area of a parallelogram.
+"""b = input('Length of base: ')
+h = input('Height of parallelogram: ')
+A = float(b)*float(h)
+print(A)
+"""
