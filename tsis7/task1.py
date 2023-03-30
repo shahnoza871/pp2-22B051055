626a81d615e9723ab4d02713fce34097d3a29e0b
shahnoza871 <s_nurubloeva@kbtu.kz>
2023-03-30 19:47:15 +0600

    Add task1

--- tsis7/task1.py
+++ tsis7/task1.py
@@ -1,38 +1,46 @@
 import pygame
+import datetime
 pygame.init()
-size = screen_width, screen_height = 850, 800
-screen = pygame.display.set_mode((size))
 
+size = screen_width, screen_height = 829, 836
+screen = pygame.display.set_mode((size))
 screen_center = screen_width/2, screen_height/2
-image = pygame.image.load("C:\\Users\\DELL\\Desktop\\mikey.png")
+
+image = pygame.image.load("C:\\Users\\DELL\\Desktop\\main_clock.png")
 img_rect = image.get_rect(center=screen_center)
 
+right_hand = pygame.image.load("C:\\Users\\DELL\\Desktop\\right_hand.png")
+right_hand_rect = right_hand.get_rect(center=screen_center)
 
-angle = 5
+left_hand = pygame.image.load("C:\\Users\\DELL\\Desktop\\left_hand.png")
+left_hand_rect = left_hand.get_rect(center=screen_center)
 
-time = pygame.time.Clock()
-
+l_angle = 5
+r_angle = 5
 
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
-    screen.fill('white')
-
-    part = pygame.Surface((70, 90))
-    part.blit(image, (screen_center), (100, 5, 80, 6))
-    part_rect = part.get_rect(center=screen_center)
 
     screen.blit(image, img_rect)
-    screen.blit(part, part_rect)
-    #screen.blit(rot1, rect1)
-    time.tick(15)
-    pygame.display.update()
 
-"""    rot1 = pygame.transform.rotate(part, angle) #
-    rect1 = rot1.get_rect()
-    rect1.center = img_rect.center
-    angle += 5"""
+    time = datetime.datetime.now()
+    minutes, seconds = time.minute, time.second
 
+    rot_l = pygame.transform.rotate(left_hand, (-6 * seconds) + 90)
+    hand_l = rot_l.get_rect()
+    hand_l.center = left_hand_rect.center
 
+    rot_r = pygame.transform.rotate(right_hand, (-6 * minutes) + 90)
+    hand_r = rot_r.get_rect()
+    hand_r.center = right_hand_rect.center
+
+    l_angle -= 0.66
+    r_angle -= 0.0011
+
+    screen.blit(rot_l, hand_l)
+    screen.blit(rot_r, hand_r)
+
+    pygame.display.flip()
